bb1e6f73cf7aea93c2c30fc6e04652570ae0575b
Deepu Mukundan <deepumukundan@gmail.com>
2015-07-28 16:53:36 -0700

    Initial Commit

---
+++ APIResponseSpoofer/APIResponseSpoofer.h
@@ -0,0 +1,19 @@
+//
+//  APIResponseSpoofer.h
+//  APIResponseSpoofer
+//
+//  Created by Deepu Mukundan on 7/28/15.
+//  Copyright (c) 2015 Hotwire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIResponseSpoofer.
+FOUNDATION_EXPORT double APIResponseSpooferVersionNumber;
+
+//! Project version string for APIResponseSpoofer.
+FOUNDATION_EXPORT const unsigned char APIResponseSpooferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIResponseSpoofer/PublicHeader.h>
+
+
