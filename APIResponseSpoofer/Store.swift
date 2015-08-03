bdba5043d84cabf0daee357bb14310d1226f3a5b
Deepu Mukundan <deepumukundan@gmail.com>
2015-08-02 23:21:21 -0700

    Some access control restrictions and new method for spoofer directory

--- APIResponseSpoofer/Store.swift
+++ APIResponseSpoofer/Store.swift
@@ -47,19 +47,32 @@ class Store {
         }
     }
 
-    class func getScenarioFilePath(scenarioName: String) -> String {
-        let documentsDirectory = applicationDocumentsDirectory()
-        let scenarioFilePath = ("\(documentsDirectory)\(scenarioName).scenario")
+    private class func getScenarioFilePath(scenarioName: String) -> String {
+        // Get a reference to the documents directory
+        let spooferDirectory = applicationDocumentsDirectory()
+        // Construct a file name based on the scenario file
+        let scenarioFilePath = ("\(spooferDirectory)\(scenarioName).scenario")
         let escapedString = scenarioFilePath.stringByReplacingOccurrencesOfString(" ", withString: "-")
         return escapedString
     }
     
-    class func applicationDocumentsDirectory() -> String {
+    private class func applicationDocumentsDirectory() -> String {
         // The directory in the application's documents directory used to store the Scenario files.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         let documentsDirectoryURL = urls[urls.count-1] as! NSURL
         let documentsDirectoryString = documentsDirectoryURL.absoluteString
         return documentsDirectoryString!
+    }
+    
+    // TODO: Currently not creating the folder. Need to debug
+    private class func spooferDocumentsDirectory() -> String {
+        let documentsDirectory = applicationDocumentsDirectory()
+        let spooferFolder = documentsDirectory.stringByAppendingPathComponent("Spoofer")
+        var isDir = ObjCBool(true)
+        if !NSFileManager.defaultManager().fileExistsAtPath(spooferFolder, isDirectory: &isDir) {
+            NSFileManager.defaultManager().createDirectoryAtPath(spooferFolder, withIntermediateDirectories: false, attributes: nil, error: nil)
+        }
+        return spooferFolder
     }
     
 }
