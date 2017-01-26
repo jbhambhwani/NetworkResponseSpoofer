397003c1de6bc72ef9007c8e6089f6275ee2117c
Deepu Mukundan <deepumukundan@gmail.com>
2017-01-25 16:16:42 -0800

    Corrected some steps

---
+++ Source/iOS_UI/View/StoryboardIdentifiers.swift
@@ -0,0 +1,27 @@
+// Auto generated file from SBConstants - any changes may be lost
+
+public enum NavigationControllerStoryboardIdentifier : String {
+    case RecordNavigationController
+    case ScenarioListNavController
+}
+
+public enum SegueIdentifier : String {
+    case EditRows
+    case showResponses
+    case unwindToRecordViewController
+}
+
+public enum StoryboardNames : String {
+    case Spoofer
+}
+
+public enum TableViewCellReuseIdentifier : String {
+    case SuiteCell
+    case SwitchWithTextTableViewCell
+    case UITableViewCell
+}
+
+public enum TableViewControllerStoryboardIdentifier : String {
+    case RecordTableViewController
+    case ScenarioListController
+}
